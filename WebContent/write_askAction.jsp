28b38e569688bf7efb1ebcdf5d324b330a4a6376
PJH6029 <top231902@naver.com>
2019-08-08 00:52:21 +0900

    writeAction 만들어야 함
    getTitle이랑 request랑 뭐써야하지

---
+++ WebContent/write_askAction.jsp
@@ -0,0 +1,95 @@
+<%@page import="java.io.File"%>
+<%@page import="java.util.Enumeration"%>
+<%@page import="com.oreilly.servlet.multipart.DefaultFileRenamePolicy"%>
+<%@page import="com.oreilly.servlet.MultipartRequest"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="bbs_ask.Bbs_askDAO" %>
+<%@ page import="java.io.PrintWriter" %>
+<% request.setCharacterEncoding("UTF-8"); %>
+<jsp:useBean id="bbs_ask" class="bbs_ask.Bbs_ask" scope="page" />
+<jsp:setProperty name="bbs_ask" property="bbsTitle"/>
+<!-- 유저아이디를 받아서 넣어줌 -->
+<jsp:setProperty name="bbs_ask" property="bbsContent"/>
+<jsp:setProperty name="bbs_ask" property="bbsImage"/>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>JSP 게시판 웹사이트</title>
+</head>
+<body>
+	<%
+		String userID = null;
+		if(session.getAttribute("userID") != null){
+			userID = (String) session.getAttribute("userID");
+		}
+		if(userID == null) {
+			PrintWriter script = response.getWriter();
+			script.println("<script>");
+			script.println("alert('로그인을 하세요.')");
+			script.println("location.href='login.jsp'");
+			script.println("</script>");
+		}
+		else {
+			String savePath = request.getRealPath("/uploadFile");
+			
+			int maxSize = 1024 * 1024 * 10;
+			
+			//제목 파일 내용
+			String bbsTitle = "";
+			String bbsContent = "";
+			
+			
+			MultipartRequest multi = null;
+			try{
+				multi = new MultipartRequest(request, savePath, maxSize, "utf-8", new DefaultFileRenamePolicy());
+				
+				bbsTitle = multi.getParameter("bbsTitle");
+				bbsContent = multi.getParameter("bbsContent");
+				
+				String fileName = multi.getFilesystemName("bbsImage");
+				
+				String bbsImageFullPath = savePath + "/" + fileName; //DB저장용
+				
+				
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+			/*
+			if(bbs_ask.getBbsTitle() == null || bbs_ask.getBbsContent() == null || bbs_ask.getBbsImage() == null){
+					PrintWriter script = response.getWriter();
+					script.println("<script>");
+					script.println("alert('입력이 안된 사항이 있습니다.')");
+					script.println("history.back()");
+					script.println("</script>");
+				} else{
+					Bbs_askDAO bbs_askDAO = new Bbs_askDAO();
+					int result = bbs_askDAO.write(bbs_ask.getBbsTitle(), userID, bbs_ask.getBbsContent());
+					
+					if (result == -1){  //오류
+						PrintWriter script = response.getWriter();
+						script.println("<script>");
+						script.println("alert('글쓰기에 실패했습니다..')");
+						script.println("history.back()");
+						script.println("</script>");
+						//아이디가 primary key이므로 똑같은거 입력되면 오류반환
+					}
+					else{
+						PrintWriter script = response.getWriter();
+						script.println("<script>");
+						script.println("location.href = 'bbs.jsp'");
+						script.println("</script>");
+					}
+					
+				}
+			*/
+			//세션:회원에 할당해주는 고유 ID
+		}
+		
+	%>
+
+
+</body>
+</html>
---
+++ src/bbs_ask/Bbs_askDAO.java
@@ -0,0 +1,158 @@
+package bbs_ask;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class Bbs_askDAO {
+	
+	private Connection conn;
+	private ResultSet rs;
+	
+	public Bbs_askDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/NULL2?serverTimezone=UTC";
+			String dbID = "root";
+			String dbPassword = "dhkd6029";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getDate() { //현재시간 가져옴
+		String SQL = "SELECT NOW()"; //현재시간
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getString(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ""; //데이터베이스 오류
+	}
+	
+	public int getNext() {
+		String SQL = "SELECT bbsID FROM BBS ORDER BY bbsID DESC"; //마지막 글의 번호가져오는 것
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getInt(1) + 1;
+			}
+			return 1; //첫번째 게시물인 경우
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+	}
+	
+	public int write(String bbsTitle, String userID, String bbsContent) { //게시글넣는 함수
+		String SQL = "INSERT INTO BBS VALUE (?, ?, ?, ?, ?, ?)"; 
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext()); 
+			pstmt.setString(2, bbsTitle); 
+			pstmt.setString(3, userID); 
+			pstmt.setString(4, getDate()); 
+			pstmt.setString(5, bbsContent); 
+			pstmt.setInt(6, 1); 
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+	}
+	
+	public ArrayList<Bbs_ask> getList(int pageNumber) { // 10개만 출력해서 보여줌
+		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10"; 
+		ArrayList<Bbs_ask> list = new ArrayList<Bbs_ask>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext() - (pageNumber - 1) * 10); // 페이지 번호지정
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				Bbs_ask bbs_ask = new Bbs_ask();
+				bbs_ask.setBbsID(rs.getInt(1));
+				bbs_ask.setBbsTitle(rs.getString(2));
+				bbs_ask.setUserID(rs.getString(3));
+				bbs_ask.setBbsDate(rs.getString(4));
+				bbs_ask.setBbsContent(rs.getString(5));
+				bbs_ask.setBbsAvailable(rs.getInt(6));
+				list.add(bbs_ask);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public boolean nextPage(int pageNumber) { //다음페이지 관리
+		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10"; 
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext() - (pageNumber - 1) * 10); // 페이지 번호지정
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+		
+	}
+	
+	public Bbs_ask getBbs_ask(int bbsID) {
+		String SQL = "SELECT * FROM BBS WHERE bbsID = ?"; 
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				Bbs_ask bbs_ask = new Bbs_ask();
+				bbs_ask.setBbsID(rs.getInt(1));
+				bbs_ask.setBbsTitle(rs.getString(2));
+				bbs_ask.setUserID(rs.getString(3));
+				bbs_ask.setBbsDate(rs.getString(4));
+				bbs_ask.setBbsContent(rs.getString(5));
+				bbs_ask.setBbsAvailable(rs.getInt(6));
+				return bbs_ask;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	public int update(int bbsID, String bbsTitle, String bbsContent) {
+		String SQL = "UPDATE BBS SET bbsTitle = ?, bbsContent = ? WHERE bbsID = ?"; 
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, bbsTitle); 
+			pstmt.setString(2, bbsContent); 
+			pstmt.setInt(3, bbsID); 
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+	}
+	
+	public int delete (int bbsID) {
+		String SQL = "UPDATE BBS SET bbsAvailable = 0 WHERE bbsID = ?"; //available 을 0으로 만들어서 눈에 안보이게. 데이터자체는 남아있음
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+	}
+}
